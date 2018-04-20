cbe8e7fc20dada2eb0b10c939ba172d17b648840
alekseevayana <alekseevayana@bk.ru>
2018-04-20 09:20:03 +0200

    Начала верстку страниц с помощью препроцессоров: исправления

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var wait = require("gulp-wait");
 
 gulp.task("style", function() {
   console.log('style builded')
@@ -16,6 +17,7 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
+    .pipe(wait(50))
     .pipe(server.stream());
 });
 
